75e52ff1aac7e0828b169252f94a7fec68008ee0
Sai Krishna Kalakonda <saikrishna.kalakonda@ibm.com>
2020-04-22 02:47:06 -0400

    creating HTTP server

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const fs = require('fs'); //brings the filesystem module refer nodejs.org docs
 const calc = require('./calc');
 const _ = require('lodash') //for javascript libraries
+const http = require('http'); //node ships with http to allow creating our own server.
 
 const colors = fs.readFileSync('colors.txt', 'UTF-8');
 //console.log(colors);
@@ -16,3 +17,20 @@ const colors2 = fs.readFile('colors.txt', 'UTF-8', function(err, data) {
 }) //this is a async function.
 
 console.log('Third');
+
+
+const server = http.createServer(function(request, response) {
+    if(request.url == '/') {
+        response.write('Hello Sai');
+        response.end();
+    }
+    if(request.url == '/numbers') {
+        response.write(JSON.stringify([1,2,3,4]));
+        response.end();
+    }
+
+    // Above we see we are doing mess with lots of urls. Inefficient way of doing. 
+    // So to create the server and manage our urls in a smarter way. we would be using EXPRESS. It is a server, again node. Gives us cleaner way of doing this.
+})
+server.listen(3100)
+console.log("Server is now running...")
