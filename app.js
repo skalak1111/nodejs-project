95e8285d1d757e583900a04812595c54bf151b93
Sai Krishna Kalakonda <saikrishna.kalakonda@ibm.com>
2020-04-22 02:29:03 -0400

    npm and external modules

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const fs = require('fs'); //brings the filesystem module refer nodejs.org docs
 const calc = require('./calc');
+const _ = require('lodash') //for javascript libraries
 
 const colors = fs.readFileSync('colors.txt', 'UTF-8');
 //console.log(colors);
-console.log(calc.name)
+//console.log(calc.name)
+const filtered = _.filter(colors, function(data) {
+    return data.indexOf('blue')
+})
